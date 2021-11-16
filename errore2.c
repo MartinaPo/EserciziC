fd7a802e5413b6f48ef518fc3284fb4957dfc5a2
MartinaPo <94382770+MartinaPo@users.noreply.github.com>
2021-11-16 18:18:09 +0100

    Update errore2.c

--- errore2.c
+++ errore2.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
+#include "elabora.h"
 
 int elabora(int contatore_parametri, char *parametri[]) {
        char buffer[4];
        strncpy (buffer, parametri[1], sizeof(buffer));
        printf("%s\n" , buffer);
-       return 0;
 }
-//commento: argv e argc possono avere qualsiasi nome, esempio: bimbogigi e goffredo
+
+/* commento: argv e argc che vediamo spesso possono avere qualsiasi nome, esempio: bimbogigi e goffredo */
 
 int main(void); {       
 int risultato;
-    char array_parametri[] = { "programma", "€" );
-    risultato = elabora(2, array_parametri);
-return risultato;
+    char *array_parametri[] = { "programma", "€" );
+    elabora(2, array_parametri);
+return 0;
 }
