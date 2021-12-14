cae576a202a154c1e324a52bd31dc7391b6a7975
Martina <94382770+MartinaPo@users.noreply.github.com>
2021-12-14 15:02:05 +0100

    Delete 20211213 directory

--- 20211213/fatt.c
+++
@@ -1,6 +0,0 @@
-#INCLUDE "fatt.h"
-
-int fatt(int n) { 
-   if (n==1) {return 1;}
-   else {return (n * fatt(n-1));}
- }
--- 20211213/fatt.h
+++
@@ -1,6 +0,0 @@
-#ifndef __FATT_H__ //se quiz.h non è definito
-#define __FATT_H__ //allora definisco quiz.h 
-
-INT fatt (INT n);
-
-#endif //fine
