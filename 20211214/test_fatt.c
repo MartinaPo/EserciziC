dc43ec2d5e37902ebf94c330b60f5f7d8f42d9c8
Martina <94382770+MartinaPo@users.noreply.github.com>
2021-12-14 15:32:59 +0100

    Create test_fatt.c

---
+++ 20211214/test_fatt.c
@@ -0,0 +1,28 @@
+#include "fatt.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+unsigned long int a, b;
+ if (argc != 3) {
+     printf("Usage: test driver <int_a> <int_b>\n");
+     exit(-1);
+ }
+
+a = atol(argv[1]); 
+ if (a == 0) {
+     printf("First argument must be an integer\n");
+     exit(-2);
+ }
+
+b = atol(argv[2]);
+
+if (b == 0) {
+     printf("Second argument must be an integer\n");
+     exit(-3);
+ }
+
+printf("fatt(%lu) = (%lu)\n", a, fatt(a));
+    return 0;
+
+}
