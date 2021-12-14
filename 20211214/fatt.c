2c2388a2c3a1b2aa2685ac977148f60ca1bd393a
Martina <94382770+MartinaPo@users.noreply.github.com>
2021-12-14 15:44:51 +0100

    Update fatt.c

--- 20211214/fatt.c
+++ 20211214/fatt.c
@@ -1,6 +1,9 @@
 #include "fatt.h"
-
-unsigned long int fatt(int n) { 
-   if (n==1) {return 1;}
-   else {return (n * fatt(n-1UL));}
+unsigned long int fatt(unsigned long long int n) { 
+   if (n==1L) {
+      return 1ULL;
+      }
+   else {
+      return (n * fatt(n-1ULL));
+   }
  }
